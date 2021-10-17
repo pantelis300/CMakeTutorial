b12b141fbd9dfabb9e746f9052383f6de82714fe
Pantelis <pantelis300@yahoo.gr>
2021-10-17 15:29:41 +0200

    Step1: Adding a Version Number and configured Header File.

--- Step1/tutorial.cpp
+++ Step1/tutorial.cpp
@@ -3,8 +3,13 @@
 #include <iostream>
 #include <string>
 
+#include "TutorialConfig.h"
+
 int main(int argc, char **argv) {
   if (argc < 2) {
+    // Report Version
+    std::cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR
+              << '.' << Tutorial_VERSION_MINOR << '\n';
     std::cout << "Usage: " << argv[0] << " number\n";
     return EXIT_FAILURE;
   }
